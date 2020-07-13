aa3a63fc8a692e53ac0af279da59c0706fb5fe94
0qy <linqianye333@gmail.com>
2020-07-13 15:48:51 +1200

    hover endNote and last bit of text format

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = { titleVisibe: true }
+    this.clickHandler = this.clickHandler.bind(this)
   }
 
   clickHandler = () => {
@@ -23,13 +24,14 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        {this.state.titleVisibe
+        {/* {this.state.titleVisibe
           ? <div id="start">
               <img id="button" onClick={this.clickHandler} src="/startIcon.jpg"/>
               <audio id="startAudio" src="startSound.mp3"/>
             </div>
           : <TextPage/>
-        }
+        } */}
+        <TextPage/>
       </div>
     )
   }
